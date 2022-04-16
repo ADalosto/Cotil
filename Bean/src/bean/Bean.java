1a93721c0795f2ff73a895e1b8c260e0e6947cae
Andrey E. Dalosto <103847726+ADalosto@users.noreply.github.com>
2022-04-16 17:18:01 -0300

    Add files via upload

---
+++ Bean/src/bean/Bean.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean;
+
+/**
+ *
+ * @author aluno
+ */
+public class Bean {
+    
+ 
+    public static void main(String[] args) {
+        int idade =17;
+        float nota = 8.0f;
+        boolean casado =false;
+        char status = 'A';
+        String nome= "Ivan";
+        System.out.println(" aluno " + nome + " tirou " + nota + " na prova");
+        System.out.println("Ele tem " + idade + " anos.");
+        System.out.println("Seu status é " + status);
+            
+    }
+}  
